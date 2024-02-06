dd5c5d4d392986a9579294d725d8ae71c5ca8792
nathandeflavis <92447278+nathandeflavis@users.noreply.github.com>
2024-02-06 19:03:59 +0000

    Added metadata to webpages useful for SEO.

--- js/head.js
+++ js/head.js
@@ -3,7 +3,9 @@ function includeHTML() {
         '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">', //use Font Awesome 4 icons
         '<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">', //use W3.css
         '<link rel="stylesheet" type="text/css" href="css/style.css">',
-        '<meta name="viewport" content="width=device-width, initial-scale=1">' //ensure page is properly displayed on mobile devices and enable touch zooming
+        '<meta name="viewport" content="width=device-width, initial-scale=1">', //ensure page is properly displayed on mobile devices and enable touch zooming
+        '<meta charset="utf-8">',
+        '<meta name="author" content="Nathan De Flavis">'
     ];
         
     html.forEach((line) => {
--- js/header.js
+++ js/header.js
@@ -2,7 +2,7 @@ function includeHTML() {
     let html = [
     '       <header class="w3-top">',
     '           <nav class="w3-bar w3-border w3-dark-grey">',
-    '               <a class="w3-bar-item w3-button">',
+    '               <a class="w3-bar-item w3-button" href="index.html">',
     '                   <img alt="Nathan." width="18" height="18" src="./images/nathan.jpeg">',
     '                   Nathan De Flavis',
     '               </a>',
